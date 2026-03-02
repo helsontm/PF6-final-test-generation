59a11baf4a9f4bee91fdd3200ab03d4b5b5dc684
Helson Tafur Manchego <helsontm@hotmail.com>
2026-03-02 12:53:58 -0500

    medio resuleto

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import json
+import requests
+
+def dish_fetch(num):
+    response = requests.get(f"https://api-colombia.com/api/v1/TypicalDish/{num}")
+    typicalDish = json.loads(response.content)
+    
+    
+    return   {
+        "id": num,
+        "name": typicalDish.get("name", f"Dish {num}")
+    }
+
+
+#print(typicalDish["description"])
+#print(typicalDish["ingredients"])
+
+
+
+
+
+
+
 def main():
   print("Hello learners!")
+  print(dish_fetch(input("Enter a number to fetch a typical dish: ")))
 
 if __name__=="__main__":
   main()
